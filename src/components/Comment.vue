162f6818fffbd898cd7087f0eefce9f6c1d8c9dd
Quim Crous <joaquimcrous@gmail.com>
2024-03-13 11:19:11 +0100

    Update - Fixed issue with comments userId and created comment component

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,7 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <p>Aixo es una proba</p>
+    <p>{{ postcomment.username }}</p>
+    <p>{{ postcomment.comment }}</p>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const comment = defineProps({
+  postcomment: Object,
+});
+</script>
 
 <style scoped></style>
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -20,6 +20,14 @@
       ></textarea>
       <button @click="postComment()">Commentar</button>
     </div>
+    <div class="md:w-3/4 w-96 pb-5 mx-auto pt-5">
+      <p>juer juer aixo no tira</p>
+      <div v-if="boolean2" v-for="comment in commentArr">
+        <p>ei?</p>
+        <Comment :postcomment="comment" />
+      </div>
+      <div v-else>lolololo</div>
+    </div>
   </div>
 </template>
 
@@ -31,14 +39,18 @@ import Navbar from "../components/Navbar.vue";
 import Post from "../components/Post.vue";
 import { useUserStore } from "../stores/user";
 import { useCommentStore } from "../stores/comments";
+import Comment from "../components/Comment.vue";
 
 const route = useRoute();
 const selectedPost = ref(null);
 const boolean = ref(false);
+const boolean2 = ref(false);
 const username = ref("");
 const userStore = useUserStore();
 const comment = ref("");
 const userId = ref(null);
+const userProfile = ref(null);
+const commentArr = ref(null);
 
 const getPostById = async () => {
   const postId = route.params.postId;
@@ -50,11 +62,25 @@ const getPostById = async () => {
   boolean.value = true;
 };
 
+const getComments = async () => {
+  const postId = route.params.postId;
+  const response = await useCommentStore().fetchComments(postId);
+  commentArr.value = response;
+  console.log("entro aqui?");
+  console.log("entro aqui?", commentArr.value);
+  for (let index = 0; index < commentArr.value.length; index++) {
+    console.log(commentArr.value[index]);
+  }
+  boolean2.value = true;
+  console.log("entro aqui?", boolean2.value);
+};
+
 async function getUser() {
   await userStore.fetchUser();
   try {
     username.value = userStore.user.data.user.email;
-    //userId.value = userStore.user.data.user.id;
+    userId.value = userStore.profile.id;
+    userProfile.value = userStore.profile.image_src;
   } catch (error) {
     username.value = "";
   }
@@ -62,7 +88,8 @@ async function getUser() {
 
 const postComment = async () => {
   const postId = route.params.postId;
-  console.log("pero pero", userId.value);
+  console.log("pero pero", username.value);
+  console.log("yo ya no se que mas", userId.value);
 
   await useCommentStore().addComment(
     postId,
@@ -77,6 +104,7 @@ const postComment = async () => {
 onMounted(() => {
   getPostById();
   getUser();
+  getComments();
 });
 </script>
 
