e1de63ff9c1658d9cb96cd367a199017e36f8ec3
Quim Crous <joaquimcrous@gmail.com>
2024-05-15 11:22:17 +0200

    Update - Created PostPreview

---
+++ src/components/PostPreview.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="border-2 rounded-md py-5 px-5 border-sky-800 bg-sky-800 text-white mb-5"
+  >
+    <h2 class="text-5xl mb-16 text-center">{{ post.title }}</h2>
+    <div class="flex justify-center mb-16">
+      <img
+        :src="post.images[0]"
+        class="h-auto rounded-lg"
+        alt=""
+        v-if="post.images"
+      />
+    </div>
+
+    <ul class="flex flex-wrap items-center justify-center">
+      <li
+        class="mr-4 hover:underline md:mr-6 capitalize"
+        v-for="tag in post.tags"
+      >
+        {{ tag }}
+      </li>
+    </ul>
+
+    <RouterLink :to="`/post/${post.id}`" @click.native="scrollToTop"
+      >Link al post</RouterLink
+    >
+  </div>
+</template>
+
+<script setup>
+import DeltaComp from "./DeltaComp.vue";
+import { RouterLink, RouterView } from "vue-router";
+const postObj = defineProps({
+  post: Object,
+});
+
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar.vue";
 import { usePostStore } from "../stores/posts";
 import DeltaComp from "../components/DeltaComp.vue";
 import Post from "../components/Post.vue";
+import PostPreview from "../components/PostPreview.vue";
 
 const posts = ref([]);
 
@@ -21,7 +22,7 @@ getPost();
     class="md:h-full bg-sky-900 bg-opacity-50 flex flex-col justify-start flex-wrap content-center pt-5 w-full"
   >
     <div class="md:w-3/4 w-96" v-for="post in posts" :key="post.id">
-      <Post :post="post" />
+      <PostPreview :post="post" />
     </div>
   </div>
 </template>
